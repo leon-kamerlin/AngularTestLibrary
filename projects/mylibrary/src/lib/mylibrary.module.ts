f20d69cdd492a27320d016fc2210fb77379bbeae
leonkamerlinn <leonkamerlin@MacBook-Pro.local>
2021-02-24 22:35:44 +0100

    ui

--- projects/mylibrary/src/lib/mylibrary.module.ts
+++ projects/mylibrary/src/lib/mylibrary.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MylibraryComponent } from './mylibrary.component';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 
 
 @NgModule({
   declarations: [MylibraryComponent],
   imports: [
+    NgxAuthFirebaseUIModule
   ],
   exports: [MylibraryComponent]
 })
